8ebf57856411804e0c4b5495469c144bf921cb8c
fisker <lionkay@gmail.com>
2019-02-22 18:53:41 +0800

    chore(prettier): prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,24 +1,104 @@
+/*!
+ * config file for prettier
+ * project https://github.com/xwtec/dotfiles
+ * primary link https://raw.githubusercontent.com/xwtec/dotfiles/master/prettier/prettier.config.js
+ *
+ * options https://prettier.io/docs/en/options.html
+ *
+ */
+
 module.exports = {
+  // options for all files
+  trailingComma: 'es5',
   semi: false,
   singleQuote: true,
   bracketSpacing: false,
+  htmlWhitespaceSensitivity: 'ignore',
 
+  // overrides
   overrides: [
     {
-      files: '*.js',
+      files: '*.{js,mjs}',
       options: {
-        parser: 'babylon',
-        semi: false,
-        singleQuote: true,
-        bracketSpacing: false
-      }
+        parser: 'babel',
+      },
     },
     {
-      files: '*.{css,scss}',
+      files: '*.ts',
+      options: {
+        parser: 'typescript',
+      },
+    },
+    {
+      files: '*.css',
       options: {
         parser: 'css',
-        singleQuote: false
-      }
-    }
-  ]
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.scss',
+      options: {
+        parser: 'scss',
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.less',
+      options: {
+        parser: 'less',
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.css',
+      options: {
+        parser: 'css',
+      },
+    },
+    {
+      files: '*.vue',
+      options: {
+        parser: 'vue',
+      },
+    },
+    {
+      files: '*.{html,htm}',
+      options: {
+        parser: 'html',
+      },
+    },
+    {
+      files: '*.json',
+      options: {
+        parser: 'json',
+      },
+    },
+    {
+      files: '*.json5',
+      options: {
+        parser: 'json5',
+      },
+    },
+    {
+      files: '*.{md,markdown}',
+      options: {
+        parser: 'markdown',
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.{yaml,yml}',
+      options: {
+        parser: 'yaml',
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.{graphql,gql}',
+      options: {
+        parser: 'graphql',
+      },
+    },
+  ],
 }
